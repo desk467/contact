a85826a26c8a02cc986d81a95c0653d76ade67f6
Ricardo Gomes <desk467@gmail.com>
2017-03-04 22:52:25 -0300

    sending component.js v0.0.1

--- component.js
+++ component.js
@@ -1,9 +1,28 @@
 contact.extend('Component', function(properties){
   return {
-    context: {},
     properties: properties,
     merge: function(anotherProperties){
       return Object.assign(properties, anotherProperties)
+    },
+    compile: function(){
+      // Do some magic here ...
+    },
+    render: function(){
+      this.compile();
+
+      var el = document.querySelector(properties.selector);
+      if(properties.template.startsWith('#')){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              el.innerHTML = properties.template;
+            }
+        };
+        xhttp.open("GET", properties.template.slice(1), true);
+        xhttp.send();
+      } else {
+        el.innerHTML = properties.template;
+      }
     }
   }
 })
